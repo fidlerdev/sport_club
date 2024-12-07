c3b15ff726ecf01fb563668b04a2f2083f307ac2
Veniamin Seraputskiy <fidlerdev@yandex.ru>
2024-12-08 02:51:51 +0300

    snap

---
+++ bot/dialog/admin.py
@@ -0,0 +1,13 @@
+import operator
+from aiogram_dialog import Dialog, Window
+from aiogram_dialog.widgets.text import Const, Format
+from aiogram_dialog.widgets.kbd import Cancel, SwitchTo, Button, ScrollingGroup, Select
+
+from bot.state import MembershipListSG
+
+
+dialog = Dialog(
+
+    Window()
+
+)
