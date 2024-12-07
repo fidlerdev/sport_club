dbdbad8963a75e0c39fe0afff98a37f4069cd51f
Veniamin Seraputskiy <fidlerdev@yandex.ru>
2024-12-07 22:55:00 +0300

    ытфз

--- config.py
+++ config.py
@@ -7,11 +7,17 @@ load_dotenv()
 DROP_TABLES = False
 
 BOT_TOKEN = os.environ["BOT_TOKEN"]
-DB_USER = os.environ["DB_USER"]
-DB_PASS = os.environ["DB_PASS"]
-DB_HOST = os.environ["DB_HOST"]
-DB_PORT = os.environ["DB_PORT"]
-DB_NAME = os.environ["DB_NAME"]
 
-# DATA SOURCE NAME
-DB_DSN = f"mysql://{DB_USER}:{DB_PASS}@{DB_HOST}:{DB_PORT}/{DB_NAME}"
+USE_MYSQL = os.environ.get("USE_MYSQL", False)
+
+if USE_MYSQL:
+    DB_USER = os.environ["DB_USER"]
+    DB_PASS = os.environ["DB_PASS"]
+    DB_HOST = os.environ["DB_HOST"]
+    DB_PORT = os.environ["DB_PORT"]
+    DB_NAME = os.environ["DB_NAME"]
+
+    # DATA SOURCE NAME
+    DB_DSN = f"mysql://{DB_USER}:{DB_PASS}@{DB_HOST}:{DB_PORT}/{DB_NAME}"
+else:
+    DB_DSN = "sqlite:////database.db"
