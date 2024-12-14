16c504b988067e8da66cee6ee7d28be406357d67
fidlerdev <fidlerdev@yandex.ru>
2024-12-14 10:21:45 +0000

    ok

--- database/schema.py
+++ database/schema.py
@@ -1,6 +1,6 @@
 from datetime import date, datetime
 from typing import Optional
-from sqlalchemy import ForeignKey
+from sqlalchemy import ForeignKey, String
 from sqlalchemy.orm import mapped_column, Mapped, Relationship
 
 from .database import Base
@@ -10,18 +10,18 @@ class UserDB(Base):
     __tablename__ = "user"
 
     id: Mapped[int] = mapped_column(primary_key=True, unique=True)
-    full_name: Mapped[Optional[str]] = mapped_column(nullable=True)
+    full_name: Mapped[Optional[str]] = mapped_column(String(300), nullable=True)
     birthday: Mapped[Optional[date]] = mapped_column(nullable=True)
-    phone: Mapped[Optional[str]] = mapped_column(nullable=True)
+    phone: Mapped[Optional[str]] = mapped_column(String(300), nullable=True)
 
-    role_id: Mapped[int] = mapped_column(ForeignKey("user_role.id", ondelete="SET NULL"))
+    role_id: Mapped[Optional[int]] = mapped_column(ForeignKey("user_role.id", ondelete="SET NULL"))
 
 
 class UserRoleDB(Base):
     __tablename__ = "user_role"
 
     id: Mapped[int] = mapped_column(primary_key=True, autoincrement=True)
-    name: Mapped[str]
+    name: Mapped[str] = mapped_column(String(300))
     value: Mapped[int] = mapped_column(unique=True)
 
 
@@ -29,10 +29,10 @@ class UserBanDB(Base):
     __tablename__ = "user_ban"
 
     id: Mapped[int] = mapped_column(primary_key=True, autoincrement=True)
-    reason: Mapped[str]
+    reason: Mapped[str] = mapped_column(String(300))
     expiration_date: Mapped[datetime]
 
-    user_id: Mapped[int] = mapped_column(ForeignKey("user.id", ondelete="CASCADE"))
+    user_id: Mapped[Optional[int]] = mapped_column(ForeignKey("user.id", ondelete="CASCADE"))
 
 
 class MemberToMembershipDB(Base):
@@ -49,8 +49,8 @@ class MembershipDB(Base):
     __tablename__ = "membership"
 
     id: Mapped[int] = mapped_column(primary_key=True, autoincrement=True)
-    name: Mapped[str]
-    description: Mapped[str]
+    name: Mapped[str] = mapped_column(String(300))
+    description: Mapped[str] = mapped_column(String(300))
     level: Mapped[int]
     days: Mapped[int]
     price: Mapped[int]
@@ -60,7 +60,7 @@ class MembershipLevelDB(Base):
     __tablename__ = "membership_level"
 
     id: Mapped[int] = mapped_column(primary_key=True, autoincrement=True)
-    name: Mapped[str]
+    name: Mapped[str] = mapped_column(String(300))
     value: Mapped[int] = mapped_column(unique=True)
 
 
@@ -77,10 +77,10 @@ class TrainingDB(Base):
 
     id: Mapped[int] = mapped_column(primary_key=True, autoincrement=True)
     scheduled_datetime: Mapped[datetime]
-    intensite: Mapped[str]
+    intensite: Mapped[str] = mapped_column(String(300))
     # В минутах
     duration: Mapped[int]
-    description: Mapped[str]
+    description: Mapped[str] = mapped_column(String(300))
     finished: Mapped[bool] = mapped_column(default=True)
 
     trainer_to_member_id: Mapped[int] = mapped_column(ForeignKey("trainer_to_member.id", ondelete="CASCADE"))
